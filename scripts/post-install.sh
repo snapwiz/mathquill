f1aaf060d6986eafbd59ce71d0f3a45571826eb9
buildsnapwiz <build@snapwiz.com>
2020-03-31 15:41:14 +0530

    update step for cache invalidate
    update step for invalidate cache from the cloud front.

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -3,3 +3,4 @@ cd ~/edulastic-mathquill
 chmod +x  script/*
 make
 aws s3 sync ~/edulastic-mathquill/build s3://edupoc/edulasticv2-development/JS/thirdpartylib/mathquill/v0.10.1
+aws cloudfront create-invalidation  --distribution-id E37FX6USRXNIY0 --paths "/edulasticv2-development/JS/thirdpartylib/mathquill/*"
