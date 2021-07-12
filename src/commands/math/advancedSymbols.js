52a446a97c6db1da6a57ffa62723690952bb8602
pin1128 <clearg1929@gmail.com>
2021-07-12 21:04:31 +0300

    added custom commands

--- src/commands/math/advancedSymbols.js
+++ src/commands/math/advancedSymbols.js
@@ -329,30 +329,3 @@ LatexCmds.deg = LatexCmds.degree = bind(VanillaSymbol,'\\degree ','&deg;');
 
 LatexCmds.ang = LatexCmds.angle = bind(VanillaSymbol,'\\angle ','&ang;');
 LatexCmds.measuredangle = bind(VanillaSymbol,'\\measuredangle ','&#8737;');
-
-LatexCmds.square = bind(VanillaSymbol, '\\square', '<span class="mq-non-leaf mq-square" />');
-LatexCmds.newline = bind(VanillaSymbol, '\\newline', '<span class="mq-non-leaf mq-newline" />');
-
-var arrowsHtml = `
-  <span class="mq-non-leaf mq-rightleftarrows">
-    <svg width="1rem" height="1rem" viewBox="0 0 24 21" version="1.1"
-      xmlns="http://www.w3.org/2000/svg">
-      <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g transform="translate(0.823000, -0.000000)" fill="currentColor" fill-rule="nonzero">
-          <path d="M5.403,-7.47669874e-14 L5.402,4 L23.177,4 L23.177,6 L5.402,6 L5.403,10.453 L0.177,5.226 L5.403,-7.47669874e-14 Z" transform="translate(11.677000, 5.226500) scale(-1, 1) translate(-11.677000, -5.226500) "></path>
-          <path d="M17.774,10.115 L23,15.341 L17.774,20.567 L17.774,16.115 L0,16.115 L0,14.115 L17.774,14.115 L17.774,10.115 Z" transform="translate(11.500000, 15.341000) scale(-1, 1) translate(-11.500000, -15.341000) "></path>
-        </g>
-      </g>
-    </svg>
-  </span>` 
-LatexCmds.rightleftarrows = bind(VanillaSymbol, '\\rightleftarrows', arrowsHtml);
-
-var wCharHtml = `
-  <span class="mq-non-leaf">
-    <svg xmlns="http://www.w3.org/2000/svg" width="22.37" height="14.638" viewBox="0 0 22.37 14.638">
-      <path d="M8.686-9.652q.193-.654.295-1.185a4.732,4.732,0,0,0,.1-.842,1.085,1.085,0,0,0-.279-.81,1.315,1.315,0,0,0-.847-.316v-.525H12.1v.525a1.43,1.43,0,0,0-.9.563,5.272,5.272,0,0,0-.676,1.507L6.67,1.308H3.11L.686-8.687-2.188,1.308H-5.641L-8.879-10.649a8.9,8.9,0,0,0-.434-1.3,1.711,1.711,0,0,0-.408-.568,1.377,1.377,0,0,0-.552-.284v-.525h6.681v.525a1.543,1.543,0,0,0-.831.306.87.87,0,0,0-.284.7,5.408,5.408,0,0,0,.1.9q.1.568.231,1.051l1.952,7.517,3.035-11H3.861L6.52-2.392Zm-5.92-2.735H1.437l-.214.794L4.129.3H5.652l.172-.461Zm-9.062,0H-8.139L-4.718.3h1.426l.214-.547Z" transform="translate(10.273 13.33)" fill="#22242b"/>
-    </svg>
-  </span>
-`
-
-LatexCmds.W = bind(VanillaSymbol,'\\mathbb{W}', wCharHtml);
